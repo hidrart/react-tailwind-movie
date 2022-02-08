9c3ce2b24a05f9bf846ce0474597710e000862c5
Hidrart <hidayattulravli@gmail.com>
2022-02-09 00:01:35 +0700

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>
-					Edit <code>src/App.js</code> and save to reload.
+					This is a test for vercel app
 				</p>
 				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
 					Learn React
